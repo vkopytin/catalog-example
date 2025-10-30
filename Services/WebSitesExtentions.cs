93255abeb49f95e4f0222fefeea8e49e86a4e0a0
Volodymyr Kopytin <4933561+vkopytin@users.noreply.github.com>
2025-10-30 18:50:52 +0100

    Prevent recursive data

--- Services/WebSitesExtentions.cs
+++ Services/WebSitesExtentions.cs
@@ -5,7 +5,7 @@ namespace Services;
 
 public static class WebSitesExtentions
 {
-  public static WebSiteModel ToModel(this WebSiteRecord webSite)
+  public static WebSiteModel ToModel(this WebSiteRecord webSite, int index = 0)
   {
     return new(
       Id: webSite.Id,
@@ -14,7 +14,7 @@ public static class WebSitesExtentions
       Name: webSite.Name ?? string.Empty,
       HostName: webSite.HostName ?? string.Empty,
       AltHostName: webSite.AltHostName ?? string.Empty,
-      Parent: webSite.Parent?.ToModel()
+      Parent: index < 5 ? webSite.Parent?.ToModel(index + 1) : null
     );
   }
 }
