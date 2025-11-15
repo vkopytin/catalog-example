dee796ac14d8e17787cf2807e928bb9ec677e8c4
Volodymyr Kopytin <4933561+vkopytin@users.noreply.github.com>
2025-11-15 20:26:57 +0100

    Index DeletedAt to improve search

--- Db/MongoDbContext.cs
+++ Db/MongoDbContext.cs
@@ -43,6 +43,7 @@ public class MongoDbContext : DbContext
     modelBuilder.Entity<SecurityGroupRecord>().ToCollection("securityGroups");
 
     modelBuilder.Entity<WebSiteArticleRecord>().HasQueryFilter(w => w.DeletedAt == null);
+    modelBuilder.Entity<WebSiteArticleRecord>().HasIndex(w => w.DeletedAt);
 
     modelBuilder.Entity<ArticleRecord>().HasOne(a => a.Media);
     modelBuilder.Entity<ArticleRecord>().HasMany(a => a.Blocks).WithOne(b => b.Article).HasForeignKey(b => b.ArticleId);
